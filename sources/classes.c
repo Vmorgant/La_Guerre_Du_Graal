f6533903778938df535d770e85561290a6ec8cef
Vmorgant <victor.morgant@gmail.com>
2016-11-09 16:38:46 +0100

    ajout de la classe saber

---
+++ sources/classes.c
@@ -0,0 +1,10 @@
+#include"global.h"
+/**
+*\file classes.c
+*\brief Ce fichier contient les définitions de chaque classes
+*\auteur Victor Morgant
+*\version 1.0
+*\date 09/11/2016
+*/
+t_classe Sab = {3,50,3,10,5,10,10, t_attaque autoAttaque, t_attaque attaquePuissante,t_spe spe};
+//int coutPE int PVmax,int gainPA,int ATQ,int DEF,int INI, float poids.
