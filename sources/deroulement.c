1cf6c3f2652e339815aa2f87ce7365ebe93d1f03
Morgant Victor <spi2195@ic2s120-16.univ-lemans.fr>
2016-11-14 15:52:00 +0100

    analyse modifiée

---
+++ sources/deroulement.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+/**
+*\file deroulement.c
+*\brief Ce fichier contient les fonctions permettant le lancement et le déroulement de la partie
+*\author Victor Morgant
+*\version 1.0
+*\date 14/11/2016
+*/
+
+void init_partie(t_liste equipe1,t_liste equipe2){
+	int i=0,j=0;
+	int valeur_elt(t_liste * p, int * v);
+	int ini2;
+	init_liste(ordre_action);
+	en_tete(equipe1);
+	while(!hors_liste){
+		
+		if(
+	}
+		
+}
