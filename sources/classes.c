72b4ac29107246b70e8b9cbe2e3925bafd05dae2
Petit Corentin <spi2203@ic2s130-17.univ-lemans.fr>
2016-11-17 15:25:06 +0100

    ajout de la gestion des equipes avant la partie (bug pour oter le dernier perso ajouté)

--- sources/classes.c
+++ sources/classes.c
@@ -10,9 +10,9 @@
 
 t_classe tab_classes[nb_classes]= {
 	//int coutPE int PVmax,int gainPA,int ATQ,int DEF,int INI, float poids, atq 1, atq 2;
-	{"Saber",3,50,3,10,5,10,10, {3,1,1,1},{5,2,1,1}/*,ulti*/},
-	{"Archer",5,50,3,10,5,10,10, {3,1,1,1},{5,2,1,1}/*,ulti*/},
-	{"Caster",2,50,3,10,5,10,10, {3,1,1,1},{5,2,1,1}/*,ulti*/},
+	{"Saber",3,50,3,10,5,10,10, {"coup d'épée",3,1,1,1},{"double coup",5,2,1,1}/*,ulti*/},
+	{"Archer",5,50,3,10,5,10,10, {"coup d'épée",3,1,1,1},{"double coup",5,2,1,1}/*,ulti*/},
+	{"Caster",2,50,3,10,5,10,10, {"coup d'épée",3,1,1,1},{"double coup",5,2,1,1}/*,ulti*/},
 };
 
 /**
--- sources/global.h
+++ sources/global.h
@@ -23,7 +23,7 @@ typedef struct{int coutPA;float mul_ATQ;int portee;int type;}t_spe;
 *\brief boisson alcoolisée saveur tequila
 */
 
-typedef struct{int coutPA;float mul_ATQ;int portee;int type;}t_attaque;
+typedef struct{char nom[20];int coutPA;float mul_ATQ;int portee;int type;}t_attaque;
 /**
 *\struct t_attaque
 *\brief contient les caractéristiques d'une attaque
@@ -41,6 +41,13 @@ typedef struct{t_classe classe ; int joueur; int pa; int pv; int x; int y; t_eta
 *\brief contient les caractéristiques d'un personnage ainsi que son équipe
 */
 
+
+
+
+
+
+
+
 typedef struct element{t_personnage personnage; struct element* pred; struct element* succ;}t_element;
 /**
 *\struct t_personnage
@@ -52,6 +59,31 @@ typedef struct{t_element * drapeau; t_element * ec;}t_liste;
 *\struct t_liste
 *\brief création du type liste (pour gérer simultanément plusieures listes de type t_personnage
 */
+
+
+
+
+
+
+
+
+typedef struct element_noeud{t_noeud noeud; struct element_noeud* pred; struct element_noeud* succ;}t_element_noeud;
+/**
+*\struct t_element_noeud
+*\brief definition d'une liste de noeud
+*/
+
+typedef struct{t_element_noeud * drapeau; t_element_noeud * ec;}t_liste_noeud;
+/**
+*\struct t_liste_noeud
+*\brief création du type liste (pour gérer simultanément plusieures listes de type t_noeud)
+*/
+
+
+
+
+
+
 
 #define nb_classes 3
 extern t_classe tab_classes[nb_classes];
