eb8061bd2459df83248b317ed0d5feada4901140
martin lebourdais <spi2185@ic2s122-04.univ-lemans.fr>
2016-11-22 14:25:17 +0100

    Correction du déplacement et ajout du déplacement simplifié

---
+++ Ressources/deplacement_simp.h
@@ -0,0 +1,15 @@
+/**
+*\file deplacement_simp.h
+*\brief Ce fichier contient les en-tête de la version simplifiée des fonctions de déplacement sur la carte.
+*\author Martin Lebourdais
+*\version 1.0
+*\date 22/11/2016
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "global.h"
+#include "liste_chemin.h"
+#include "Init_map.h"
+
+void deplacement_simp(t_liste *ordre_action,t_map map);
