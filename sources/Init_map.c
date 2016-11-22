de665e819c33da4928b44b556e2226ed799c2e36
martin lebourdais <spi2185@ic2s122-04.univ-lemans.fr>
2016-11-22 16:10:45 +0100

    Ajout de la map

--- sources/Init_map.c
+++ sources/Init_map.c
@@ -45,7 +45,44 @@ t_map actumap(t_liste * ordre_action, t_map map){
     if (ordre_action->ec->personnage.joueur == 2){
         		if (ordre_action->ec->personnage.classe.nom == "Saber") map.cell[ordre_action->ec->personnage.x][ordre_action->ec->personnage.y] = 2;
         	}
+    if (ordre_action->ec->personnage.joueur == 1){     
+        		if (ordre_action->ec->personnoeud avec un cout inférieur au noeud en paramètre dans la liste en paramètre.
+ * \param t_liste_noeud liste : La liste dans lnage.classe.nom == "Archer") map.cell[ordre_action->ec->personnage.x][ordre_action->ec->personnage.y] = 3; // On place le personnage de la classe indiquée dans l'élément courant à ses coordonées dans la matrice.
+        	}
+    if (ordre_action->ec->personnage.joueur == 2){
+        		if (ordre_action->ec->personnage.classe.nom == "Archer") map.cell[ordre_action->ec->personnage.x][ordre_action->ec->personnage.y] = 4;
+        	}
+    if (ordre_action->ec->personnage.joueur == 1){     
+        		if (ordre_action->ec->personnage.classe.nom == "Caster") map.cell[ordre_action->ec->personnage.x][ordre_action->ec->personnage.y] = 5; // On place le personnage de la classe indiquée dans l'élément courant à ses coordonées dans la matrice.
+        	}
+    if (ordre_action->ec->personnage.joueur == 2){
+        		if (ordre_action->ec->personnage.classe.nom == "Caster") map.cell[ordre_action->ec->personnage.x][ordre_action->ec->personnage.y] = 6;
+        	}
+    if (ordre_action->ec->personnage.joueur == 1){     
+        		if (ordre_action->ec->personnage.classe.nom == "Berserker") map.cell[ordre_action->ec->personnage.x][ordre_action->ec->personnage.y] = 7; // On place le personnage de la classe indiquée dans l'élément courant à ses coordonées dans la matrice.
+        	}
+    if (ordre_action->ec->personnage.joueur == 2){
+        		if (ordre_action->ec->personnage.classe.nom == "Berserker") map.cell[ordre_action->ec->personnage.x][ordre_action->ec->personnage.y] = 8;
+        	}
+    if (ordre_action->ec->personnage.joueur == 1){     
+        		if (ordre_action->ec->personnage.classe.nom == "Lancer") map.cell[ordre_action->ec->personnage.x][ordre_action->ec->personnage.y] = 9; // On place le personnage de la classe indiquée dans l'élément courant à ses coordonées dans la matrice.
+        	}
+    if (ordre_action->ec->personnage.joueur == 2){
+        		if (ordre_action->ec->personnage.classe.nom == "Lancer") map.cell[ordre_action->ec->personnage.x][ordre_action->ec->personnage.y] = 10;
+        	}
+    if (ordre_action->ec->personnage.joueur == 1){     
+        		if (ordre_action->ec->personnage.classe.nom == "Rider") map.cell[ordre_action->ec->personnage.x][ordre_action->ec->personnage.y] = 11; // On place le personnage de la classe indiquée dans l'élément courant à ses coordonées dans la matrice.
+        	}
+    if (ordre_action->ec->personnage.joueur == 2){
+        		if (ordre_action->ec->personnage.classe.nom == "Rider") map.cell[ordre_action->ec->personnage.x][ordre_action->ec->personnage.y] = 12;
+        	}
     
+    if (ordre_action->ec->personnage.joueur == 1){     
+        		if (ordre_action->ec->personnage.classe.nom == "Assassin") map.cell[ordre_action->ec->personnage.x][ordre_action->ec->personnage.y] = 13; // On place le personnage de la classe indiquée dans l'élément courant à ses coordonées dans la matrice.
+        	}
+    if (ordre_action->ec->personnage.joueur == 2){
+        		if (ordre_action->ec->personnage.classe.nom == "Assassin") map.cell[ordre_action->ec->personnage.x][ordre_action->ec->personnage.y] = 14;
+        	}
     return map;
 }
 
@@ -71,6 +108,66 @@ void afficherMat (t_map mat){
 	    	if(mat.cell[i][j] == 2) {				 //Si la case contient une "Saber" Rouge
 	    		couleur("31;1");					 //On écrit en rouge et en gras
 	    		printf(" SAB ");
+	    		couleur("0");
+	    	}
+		if(mat.cell[i][j] == 4) {				 //Si la case contient une "Archer" Rouge
+	    		couleur("31;1");					 //On écrit en rouge et en gras
+	    		printf(" ARC ");
+	    		couleur("0");
+	    	}
+		if(mat.cell[i][j] == 3) {				 //Si la case contient un "Archer" Bleu
+	    		couleur("34;1");					 //On écrit en bleu et en gras
+	    		printf(" ARC ");
+	    		couleur("0");
+	    	}
+		if(mat.cell[i][j] == 5) {				 //Si la case contient un "Caster" Bleu
+	    		couleur("34;1");					 //On écrit en bleu et en gras
+	    		printf(" CAS ");
+	    		couleur("0");
+	    	}
+		if(mat.cell[i][j] == 6) {				 //Si la case contient un "Caster" Rouge
+	    		couleur("31;1");					 //On écrit en rouge et en gras
+	    		printf(" CAS ");
+	    		couleur("0");
+	    	}
+		if(mat.cell[i][j] == 7) {				 //Si la case contient un "Berzerker" Bleu
+	    		couleur("34;1");					 //On écrit en bleu et en gras
+	    		printf(" BER ");
+	    		couleur("0");
+	    	}
+		if(mat.cell[i][j] == 8) {				 //Si la case contient un "Berzerker" Rouge
+	    		couleur("31;1");					 //On écrit en Rouge et en gras
+	    		printf(" BER ");
+	    		couleur("0");
+	    	}
+		if(mat.cell[i][j] == 9) {				 //Si la case contient un "Lancer" Bleu
+	    		couleur("34;1");					 //On écrit en bleu et en gras
+	    		printf(" LAN ");
+	    		couleur("0");
+	    	}
+		if(mat.cell[i][j] == 10) {				 //Si la case contient un "Lancer" Rouge
+	    		couleur("31;1");					 //On écrit en Rouge et en gras
+	    		printf(" LAN ");
+	    		couleur("0");
+	    	}
+		if(mat.cell[i][j] == 11) {				 //Si la case contient un "Rider" Bleu
+	    		couleur("34;1");					 //On écrit en bleu et en gras
+	    		printf(" RID ");
+	    		couleur("0");
+	    	}
+		if(mat.cell[i][j] == 12) {				 //Si la case contient un "Rider" Rouge
+	    		couleur("31;1");					 //On écrit en Rouge et en gras
+	    		printf(" RID ");
+	    		couleur("0");
+	    	}
+		if(mat.cell[i][j] == 13) {				 //Si la case contient un "Assassin" Bleu
+	    		couleur("34;1");					 //On écrit en bleu et en gras
+	    		printf(" ASS ");
+	    		couleur("0");
+	    	}
+		if(mat.cell[i][j] == 14) {				 //Si la case contient un "Assassin" Rouge
+	    		couleur("31;1");					 //On écrit en Rouge et en gras
+	    		printf(" ASS ");
 	    		couleur("0");
 	    	}
 			
