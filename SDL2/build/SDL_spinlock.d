f6b1868a0bee21eba276f324c224a96b2bce76e0
corentin petit <spi2203@ic2s114-08.univ-lemans.fr>
2016-11-29 13:09:35 +0100

    Fallait pas mettre la SDL là

--- SDL2/build/SDL_spinlock.d
+++
@@ -1,9 +0,0 @@
-build/SDL_spinlock.lo: \
- /home/victor/Documents/LGDG/SDL2/src/atomic/SDL_spinlock.c \
- /home/victor/Documents/LGDG/SDL2/src/atomic/../SDL_internal.h \
- /home/victor/Documents/LGDG/SDL2/src/atomic/../dynapi/SDL_dynapi.h \
- /home/victor/Documents/LGDG/SDL2/src/atomic/../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_atomic.h include/SDL_stdinc.h \
- include/SDL_config.h include/SDL_mutex.h include/SDL_error.h \
- include/SDL_timer.h
