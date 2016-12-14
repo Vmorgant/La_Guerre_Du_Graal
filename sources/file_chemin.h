aaf50ae2f82cfe4fef309b98f8a72adef8e03dbd
victor morgant <spi2195@ic2s122-07.univ-lemans.fr>
2016-12-14 16:24:06 +0100

    documentationfile_chemin et ges_equipes

--- sources/file_chemin.h
+++ sources/file_chemin.h
@@ -1,4 +1,10 @@
-
+/**
+*\file file_chemin.h
+*\brief Ce fichier contient les définitions des primitives d'acces aux files de noeuds
+*\author Martin Lebourdais
+*\version 2.0
+*\date 01/12/2016
+*/
 #include<stdio.h>
 #include<stdlib.h>
 #include "global.h"
