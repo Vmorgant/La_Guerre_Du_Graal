241fa1608d6ddb5e78b62dedf2185a5cc7610dad
victor morgant <spi2195@ic2s114-19.univ-lemans.fr>
2016-12-15 16:33:47 +0100

    sdl v1

--- sources/main.c
+++ sources/main.c
@@ -17,7 +17,22 @@
 #include"deplacement_simp.h"
 #include"ges_partie.h"
 #include "save.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
+int init_SDL(){
+	  /* Initialisation de la SDL */
+	if (SDL_Init(SDL_INIT_VIDEO) != 0 ){
+       		fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
+		return -1;
+	}
+	if( TTF_Init() !=0){
+		fprintf(stdout,"Échec de l'initialisation de la SDLttf (%s)\n",TTF_GetError()); 
+		return -2;
+	}
+	fprintf(stdout,"Init OK\n"); 
+	return 1;
+}
 void clearScreen(){
 	system("clear");
 }
@@ -129,7 +144,61 @@ void lancer1v1() {
 
 
 int main(void) {
-	int choix, erreur = faux;
+	int choix, erreur = faux,sdl = 0,i;
+
+	sdl=init_SDL();
+	if (sdl == 1){
+		SDL_Event event;
+		SDL_DisplayMode current;
+		const Uint8 *keystates = SDL_GetKeyboardState(NULL);/*stockage de l'état des touches du clavier*/
+		SDL_Surface* Fond =SDL_LoadBMP("../Ressources/images/menu_principal.bmp");// chargement de l'image du menu principal
+		TTF_Font *police=NULL; //stockage de la police
+		police = TTF_OpenFont("../Ressources/police/angelina.ttf", 63);
+		SDL_Color textColor = {255,255,255};//blanc
+		SDL_Surface *message, *message2= NULL; 
+
+		/*Chargement de la résolution courante de l'écran*/
+		for(i = 0; i < SDL_GetNumVideoDisplays(); ++i){
+			int error = SDL_GetCurrentDisplayMode(i, &current);
+			if(error != 0)
+      				SDL_Log("Could not get display mode for video display #%d: %s", i, SDL_GetError());
+			SDL_Log("Display #%d: current display mode is %dx%dpx @ %dhz.", i, current.w, current.h, current.refresh_rate);
+
+  		}
+
+      		/* Création de la fenêtre a la résolution actuelle de l'écran  */
+        	SDL_Window* Fenetre = NULL;
+       		Fenetre = SDL_CreateWindow
+("LGDG",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,current.w,current.h,SDL_WINDOW_SHOWN);
+		message = TTF_RenderText_Solid( police, "La Guerre Du Graal", textColor ); 
+		message2 = TTF_RenderText_Solid( police, "Appuyez sur espace pour continuer ou echap pour quitter", textColor );
+                SDL_Rect HG = {0,0,0,0};// Coin haut gauche du sprite en haut à gauche de la fenêtre
+		SDL_Rect C = {(current.w)/2-message->w/2,(current.h)/2-message->h/2,0,0};// centrage du texte
+                SDL_BlitSurface(Fond,NULL,SDL_GetWindowSurface(Fenetre),&HG);
+		SDL_BlitSurface(message,NULL,SDL_GetWindowSurface(Fenetre),&C);
+		/*copie dans la fenetre parametre : source,région de la source à copier(NULL si toute la source,surface de destination,emplacement */
+                SDL_UpdateWindowSurface(Fenetre);
+		/*mise à jour de la fenêtre*/
+		while ( SDL_PollEvent(&event) ){
+			if(keystates[SDL_SCANCODE_ESCAPE] ||event.window.event == SDL_WINDOWEVENT_CLOSE){
+				printf(" Au revoir\n");
+					
+                    	}
+			else if (keystates[SDL_SCANCODE_SPACE]) { 
+                       		Fond =SDL_LoadBMP("./images/menu2.bmp");
+				SDL_BlitSurface(Fond,NULL,SDL_GetWindowSurface(Fenetre),&HG);
+				SDL_UpdateWindowSurface(Fenetre);
+                    	}
+                }
+		SDL_Rect cligno = {0,(current.h)/2-message->h/2 + 50,0,0};
+		SDL_BlitSurface(message2,NULL,SDL_GetWindowSurface(Fenetre),&cligno);
+		SDL_UpdateWindowSurface(Fenetre);
+		SDL_BlitSurface(Fond,NULL,SDL_GetWindowSurface(Fenetre),&HG);
+		SDL_BlitSurface(message,NULL,SDL_GetWindowSurface(Fenetre),&C);
+		SDL_UpdateWindowSurface(Fenetre);
+		
+           
+	}
 	
 	do {		
 		clearScreen();	
@@ -163,5 +232,16 @@ int main(void) {
 	printf("Au revoir !\n");
 	sleep(2);
 	clearScreen();
+	if (sdl == 1){
+		SDL_DestroyWindow(Fenetre);
+		SDL_FreeSurface(Fond);
+		SDL_FreeSurface(message);
+		SDL_FreeSurface(message2);
+		SDL_Quit();
+		//Fermeture des Fonts qu'on a utilisé 
+		TTF_CloseFont(police); 
+		//On quitte SDL_ttf 
+		TTF_Quit();
+	}
 	return EXIT_SUCCESS;
 }
